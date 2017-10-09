47a1c4869eb6518e9df242a7b7ad7698c6af42c5
martin8990 <32632666+martin8990@users.noreply.github.com>
2017-10-09 18:23:04 +0200

    Minor Changes

---
+++ GravityAssist5/Assets/Galaxy/Operation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Entitas;
+
+public class Operation : FlowSystem
+{
+    public List<string> componentNames;
+    
+    public override void MyAction(Contexts contexts)
+    {
+        foreach (var name in componentNames)
+        {
+            contexts.universe.GetGroup()
+        }
+
+        throw new System.NotImplementedException();
+    }
+}
