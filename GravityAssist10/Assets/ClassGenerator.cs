bddf2fc23acdd7ee4b3bc3e5ec9d62a2b7a67306
martin8990 <32632666+martin8990@users.noreply.github.com>
2017-10-16 20:55:47 +0100

    New version
    Generics not working so well

---
+++ GravityAssist10/Assets/ClassGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+public class ClassGenerator : MonoBehaviour {
+    public List<string> directories = new List<string>();
+    public List<ComponentPrefab> componentLines = new List<ComponentPrefab>();
+
+    public void AddDirectory(string dir)
+    {
+        directories.Add(dir);
+    }
+    public void RemoveDirectory(string dir)
+    {
+        directories.Remove(dir); 
+    }
+  
+    
+
+
+}
+[System.Serializable]
+public class ComponentPrefab
+{
+    public string origin;
+    public List<string> lines;
+}
---
+++ GravityAssist10/Assets/FileLines.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+[System.Serializable]
+public class FileLines
+{
+    public List<string> lines = new List<string>();
+    
+    public FileLines(List<string> lines)
+    {
+        this.lines = lines;
+    }
+}
