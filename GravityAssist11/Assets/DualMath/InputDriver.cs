ba161f8668555fafa5d8fe01dcc34f246e81f23e
martin8990 <32632666+martin8990@users.noreply.github.com>
2017-11-15 13:33:23 +0100

    Restructured, Dual Math
    asd

---
+++ GravityAssist11/Assets/DualMath/InputDriver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InputDriver : MonoBehaviour {
+    public InputField inputField;
+
+    public void Start()
+    {
+        inputField.ActivateInputField();
+    }
+
+    public void OnEdit()
+    {
+        Debug.Log(inputField.text);
+    }
+
+
+}
